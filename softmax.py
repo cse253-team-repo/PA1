e5c5754c22c20f3fe4e521811917539536ab94d4
Zhenrui Yue <yuezrhb@gmail.com>
2020-01-19 23:53:51 -0800

    fix bug

--- softmax.py
+++ softmax.py
@@ -7,14 +7,14 @@ class SoftmaxRegression:
     def __init__(self):
         pass
 
-    def train_batch(self, X, y, classes, iterations=100, learning_rate=0.1):
+    def train_batch(self, X, y, classes, epoch=100, learning_rate=0.01):
         self.n_samples, self.n_features = X.shape
         self.classes = classes
 
         self.theta = np.random.rand(self.classes, self.n_features)
         self.bias = np.zeros((1, self.classes))
 
-        for i in range(iterations):
+        for i in range(epoch):
             scores = np.dot(X, self.theta.T) + self.bias
             probs = self.softmax(scores)
             y_predict = np.argmax(probs, axis=1)[:, np.newaxis]
@@ -26,38 +26,33 @@ class SoftmaxRegression:
             self.theta = self.theta - learning_rate * dw.T
             self.bias = self.bias - learning_rate * db
 
-            if i % 100 == 0:
-                print('Iteration:', i)
-
         return self.theta, self.bias
 
-    def train_stochastic(self, X, y, classes, iterations=100, learning_rate=0.1):
+    def train_stochastic(self, X, y, classes, epoch=100, learning_rate=0.01):
         self.n_samples, self.n_features = X.shape
         self.classes = classes
 
         self.theta = np.random.rand(self.classes, self.n_features)
         self.bias = np.zeros((1, self.classes))
 
-        for i in range(iterations):
-            scores = np.dot(X, self.theta.T) + self.bias
-            probs = self.softmax(scores)
-            y_one_hot = self.one_hot(y)
+        for i in range(epoch):
+            random_order = np.arange(self.n_samples)
+            np.random.shuffle(random_order)
 
-            rand1 = np.random.randint(0, self.n_samples, 1)
-            h = np.dot(self.theta, X[rand1].T)
-            for i in range(self.n_features):
-                self.theta[:, i] = self.theta[:, i] - \
-                    (1 / self.n_samples) * learning_rate * \
-                    (h.T - y_one_hot[rand1]) * X[rand1, i].item(0)
+            for j in range(len(random_order)):
+                scores = np.dot(X, self.theta.T) + self.bias
+                probs = self.softmax(scores)
+                y_one_hot = self.one_hot(y)
 
-            #dw = (1 / self.n_samples) * np.dot(X.T, (probs - y_one_hot))
-            db = (1 / self.n_samples) * np.sum(probs - y_one_hot, axis=0)
+                rand = random_order[j]
+                h = np.dot(self.theta, X[rand].T)
+                dw = (1 / self.n_samples) * \
+                    np.outer(h.T - y_one_hot[rand], X[rand])
+                self.theta = self.theta - learning_rate * dw
 
-            #self.theta = self.theta - learning_rate * dw.T
-            self.bias = self.bias - learning_rate * db
-
-            if i % 100 == 0:
-                print('Iteration:', i)
+                db = (1 / self.n_samples) * \
+                    np.sum(probs - y_one_hot, axis=0)
+                self.bias = self.bias - learning_rate * db
 
         return self.theta, self.bias
 
@@ -82,6 +77,18 @@ class SoftmaxRegression:
         one_hot = np.zeros((self.n_samples, self.classes))
         one_hot[np.arange(self.n_samples), y.T] = 1
         return one_hot
+
+
+def cross_fold(X, fold_num=2):
+    indices = []
+    num = int(len(X) / fold_num)
+    for i in range(fold_num):
+        if i != (fold_num - 1):
+            temp_index = [i for i in range(i * num, (i + 1) * num)]
+        else:
+            temp_index = [i for i in range(i * num, len(X))]
+        indices.append(temp_index)
+    return indices
 
 
 def PCA(X, pc_num=1):
@@ -111,22 +118,22 @@ def PCA_project(X, average_data, projector, pc_num=1):
     return X_PCA
 
 
-def get_accuracy(y_trained, y_true):
-    if len(y_trained) != len(y_true):
+def get_accuracy(y_predict, y_true):
+    if len(y_predict) != len(y_true):
         print('Different Data Dimension!')
         return 0
 
     match = 0
-    for i in range(len(y_trained)):
-        if y_trained[i] == y_true[i]:
+    for i in range(len(y_predict)):
+        if y_predict[i] == y_true[i]:
             match += 1
-    return match / len(y_trained)
+    return match / len(y_predict)
 
 
 data_dir = "./aligned/"
 dataset, cnt = load_data(data_dir)
 images = balanced_sampler(dataset, cnt, emotions=[
-                          'anger', 'disgust', 'fear', 'happiness', 'sadness', 'surprise'])
+    'anger', 'disgust', 'fear', 'happiness', 'sadness', 'surprise'])
 
 emotion_num = 0
 X = []
@@ -136,14 +143,38 @@ for k in images.keys():
     y = y + [emotion_num] * len(images[k])
     emotion_num += 1
 
-X = np.matrix(X)
-y = np.matrix(y).T
 
-X_PCA, average_data, projector = PCA(X, pc_num=15)
+shuffle_order = np.arange(len(X))
+np.random.shuffle(shuffle_order)
+X = np.matrix(X)[shuffle_order]
+y = np.matrix(y).T[shuffle_order]
 
-softmax = SoftmaxRegression()
-w_trained, b_trained = softmax.train_batch(
-    X_PCA, y, classes=6, iterations=800, learning_rate=0.1)
+num_folds = 10
+folds = cross_fold(X, num_folds)
+for fold in range(num_folds):
+    X_val = X[folds[fold]]
+    y_val = y[folds[fold]]
+    X_test = X[folds[(fold + 1) % num_folds]]
+    y_test = y[folds[(fold + 1) % num_folds]]
 
-y_predict = softmax.predict(X_PCA)
-print(get_accuracy(y_predict.A1, y.A1))
+    rest_folds = []
+    for i in range(num_folds):
+        if i != fold and i != ((fold + 1) % num_folds):
+            rest_folds = rest_folds + folds[i]
+    X_train = X[rest_folds]
+    y_train = y[rest_folds]
+
+    X_train_PCA, average_data, projector = PCA(X_train, pc_num=40)
+    X_test_PCA = PCA_project(X_test, average_data, projector, pc_num=40)
+    X_val_PCA = PCA_project(X_val, average_data, projector, pc_num=40)
+
+    softmax = SoftmaxRegression()
+    w_trained, b_trained = softmax.train_stochastic(
+        X_train_PCA, y_train, classes=6, epoch=100, learning_rate=0.1)
+
+    y_train_predict = softmax.predict(X_train_PCA)
+    y_test_predict = softmax.predict(X_test_PCA)
+    y_val_predict = softmax.predict(X_val_PCA)
+    print('Training Accuracy:', get_accuracy(y_train_predict.A1, y_train.A1))
+    print('Testing Accuracy:', get_accuracy(y_test_predict.A1, y_test.A1))
+    print('Validation Accuracy:', get_accuracy(y_val_predict.A1, y_val.A1))
